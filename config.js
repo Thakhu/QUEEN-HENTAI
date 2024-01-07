008082368813ebd05bc28288343e692f55d6f0dd
Thakhu <154768771+Thakhu@users.noreply.github.com>
2024-01-07 15:21:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = [
+global.owner = [Tk
   ['94767939688', 'Dinuwa Official²⁰²³', true],
   ['94767939688', 'Dinuwa Official²⁰²³', true], 
   [''] 
-] //Number of owners
+0712759107] //Number of owners
 
 global.mods = ['94767939688','94767939688'] 
 global.prems = ['94767939688', '94767939688', '94767939688']
